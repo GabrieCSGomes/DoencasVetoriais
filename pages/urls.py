55faaa7725119b751ad7e65c8a836dbf348b393a
mjneto <manoelneto.snts@gmail.com>
2022-06-25 21:49:56 -0300

    Adição de visualização, alteração e exclusão de fichas
    - Remoção do base.css duplicado
    - Modificação nas url (incluir novas páginas e adequação)
    - Modificação nas views (incluir novas funcionalidades e adequar existentes)
    - Coleta do base.css
    - Adição (e modificação) dos templates para as funcionalidades
    
    TODO: Formatar tabela dos dados da ficha, melhorar aparência

--- pages/urls.py
+++ pages/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from .views import FichaRegistroView, HomePageView
+from .views import FichaDeleteView, FichaListView, FichaCreateView, FichaUpdateView, HomePageView, FichaDetailView
 
 urlpatterns = [
+    path('ficha/lista/', FichaListView.as_view(), name='ficha_list'),
+    path('ficha/<int:pk>/editar/', FichaUpdateView.as_view(), name='ficha_update'),
+    path('ficha/<int:pk>/deletar/', FichaDeleteView.as_view(), name='ficha_delete'),
     path('', HomePageView.as_view(), name='home'),
-    path('ficha/', FichaRegistroView.as_view(), name='ficha'),
-    #path('ficha/<int:pk>/', FichaDetailView.as_view(), name='ficha_detail')
+    path('ficha/cadastro/', FichaCreateView.as_view(), name='ficha_create'),
+    path('ficha/<int:pk>/detalhes/', FichaDetailView.as_view(), name='ficha_read'),
 ]
--- pages/views.py
+++ pages/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponseRedirect
+from django.contrib import messages
+from django.urls import reverse
 from django.shortcuts import render
-from django.views.generic import TemplateView, View, DetailView
+from django.views.generic import TemplateView, DetailView, CreateView, ListView, UpdateView, DeleteView
 from pages.forms import DistritosAdmForm, FichaForm, EnderecoForm, InformacoesForm, EquipeForm
 from pages.models import DistritosAdm, Endereco, Equipe, FichaVisita, Info
 
@@ -8,18 +10,18 @@ from pages.models import DistritosAdm, Endereco, Equipe, FichaVisita, Info
 class HomePageView(TemplateView):
     template_name = 'home.html'
 
-'''class FichaDetailView(DetailView):
+class FichaDetailView(DetailView): #Detalhes de uma ficha
     model = FichaVisita
-    template_name = 'ficha_detail.html'
+    template_name = 'crud/ficha_read.html'
 
-class FichaListView(View):
+class FichaListView(ListView): # Lista todas as fichas
     model = FichaVisita
-    template_name = 'ficha_list.html'''
+    template_name = 'ficha_list.html'
 
-class FichaRegistroView(View):
+class FichaCreateView(CreateView): #Criar uma nova ficha
     def get(self, request):
         context = {'ficha_form': FichaForm(), 'distrito_form': DistritosAdmForm(), 'endereco_form': EnderecoForm(), 'informacoes_form': InformacoesForm(), 'equipe_form': EquipeForm()}
-        return render(request, 'ficha.html', context)
+        return render(request, 'crud/ficha_create.html', context)
 
     def post(self, request):
         if request.method == 'POST':
@@ -51,10 +53,67 @@ class FichaRegistroView(View):
                 new_ficha.infos = Info.objects.get(id=new_informacoes.id)
                 new_ficha.save()
                 
-                return HttpResponseRedirect('/')
+                messages.success(request, 'Ficha cadastrada com sucesso!')
+
+                return HttpResponseRedirect(reverse('ficha_read', kwargs={'pk': new_ficha.id}))
             else:
                 context = {'ficha_form': ficha_form, 'distrito_form': distrito_form, 'endereco_form': endereco_form, 'informacoes_form': informacoes_form, 'equipe_form': equipe_form}
         else:
             context = {'ficha_form': FichaForm(), 'distrito_form': DistritosAdmForm(), 'endereco_form': EnderecoForm(), 'informacoes_form': InformacoesForm(), 'equipe_form': EquipeForm()}
 
         return render(request, 'ficha.html', context)
+
+class FichaUpdateView(UpdateView): # Editar uma ficha e objetos relacionados
+    model = FichaVisita
+    template_name = 'crud/ficha_update.html'
+    
+    def post(self, request, pk):
+        ficha = FichaVisita.objects.get(id=pk)
+        
+        if request.method == 'POST':
+            ficha_form = FichaForm(request.POST, instance=ficha)
+            distrito_form = DistritosAdmForm(request.POST, instance=ficha.distrito)
+            endereco_form = EnderecoForm(request.POST, instance=ficha.endereco)
+            informacoes_form = InformacoesForm(request.POST, instance=ficha.infos)
+            equipe_form = EquipeForm(request.POST, instance=ficha.infos.equipe)
+        if ficha_form.is_valid() and distrito_form.is_valid() and endereco_form.is_valid() and informacoes_form.is_valid() and equipe_form.is_valid():
+            ficha_form.save()
+            distrito_form.save()
+            endereco_form.save()
+            informacoes_form.save()
+            equipe_form.save()
+            messages.success(self.request, 'Ficha atualizada com sucesso!')
+            return HttpResponseRedirect(reverse('ficha_read', kwargs={'pk': ficha.id}))
+        else:
+            messages.error(self.request, 'Erro ao atualizar ficha!')
+            return HttpResponseRedirect(reverse('ficha_update', kwargs={'pk': ficha.id}))
+            
+    def get_context_data(self, **kwargs):
+        context = {
+            'ficha_form': FichaForm(instance=self.object),
+            'distrito_form': DistritosAdmForm(instance=self.object.distrito),
+            'endereco_form': EnderecoForm(instance=self.object.endereco),
+            'informacoes_form': InformacoesForm(instance=self.object.infos),
+            'equipe_form': EquipeForm(instance=self.object.infos.equipe)}
+        return context
+
+class FichaDeleteView(DeleteView): # Deletar uma ficha e objetos associados
+    model = FichaVisita
+    template_name = 'crud/ficha_delete.html'
+
+    def delete(self, request, pk):
+        ficha = FichaVisita.objects.get(id=pk)
+        if ficha.distrito:
+            ficha.distrito.delete()
+        if ficha.endereco:
+            ficha.endereco.delete()
+        if ficha.infos:
+            ficha.infos.delete()
+            ficha.infos.equipe.delete()
+        ficha.delete()
+
+        messages.success(self.request, 'Ficha deletada com sucesso!')
+        return HttpResponseRedirect(reverse('ficha_list'))
+
+    def get_success_url(self):
+        return reverse('ficha_list')
